ec02a166a494b08248069ddaee61a28894ce4900
Sam Burgett <samwise.burgett@outlook.com>
2024-04-07 12:01:39 -0400

    added another else if to the input alert

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function empty() {
     if (number.value.length <= 0) {
         alert("Please enter a valid number");
     } else if (number.value <= -1){
-        alert("Please enter a number greater than or equal to 1")
+        alert("Please enter a number greater than or equal to 1");
+    } else if (number.value >= 4000) {
+        alert("Please enter a number less than or equal to 3999");
     }
 }
