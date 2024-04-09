2d05de9f73795a47f03a2df6699573c109f18d82
Sam Burgett <samwise.burgett@outlook.com>
2024-04-09 17:44:12 -0400

    added function to display I and IV as needed

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function empty() {
-    if (number.value.length <= 0) {
+const numberValue = document.getElementById("number");
+const output = document.getElementById("output");
+const convertBtn = document.getElementById("convert-btn")
+const ones = "I";
+const fives = "V"
+
+function isInputEmpty() {
+    if (numberValue.value.length <= 0) {
         alert("Please enter a valid number");
-    } else if (number.value <= -1){
+    } else if (numberValue.value <= -1){
         alert("Please enter a number greater than or equal to 1");
-    } else if (number.value >= 4000) {
+    } else if (numberValue.value >= 4000) {
         alert("Please enter a number less than or equal to 3999");
+    }
+}
+
+function firstDigit() {
+    if(numberValue.value.length === 1 && numberValue.value <= 3) {
+        output.innerText = ones;
+        for (let i = 1; i < output.value; i++) {
+            output.innerText += "I";
+        }
+    }  else if (numberValue.value.length === 1 && parseInt(numberValue.value) === 4) {
+        output.innerText = ones + fives;
     }
 }
